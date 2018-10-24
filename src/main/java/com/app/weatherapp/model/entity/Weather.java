c0b86067405e992ef1a233fc407c857ef196462c
Stanislav Cheslavskyi <stas.hess@gmail.com>
2018-10-24 10:19:24 +0300

    weather entity

--- src/main/java/com/app/weatherapp/model/entity/Weather.java
+++ src/main/java/com/app/weatherapp/model/entity/Weather.java
@@ -1,4 +1,47 @@
 package com.app.weatherapp.model.entity;
 
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "weather",uniqueConstraints = {@UniqueConstraint(columnNames = "date",name = "unique_date")})
 public class Weather {
+    @Id
+    @SequenceGenerator(name = "weather_seq", sequenceName = "weather_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "weather_seq")
+    private Long id;
+
+    @Column(name = "date",nullable = false)
+    private LocalDateTime date;
+
+    @Column(name = "temp",nullable = false)
+    private Double temp;
+
+    public Weather() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Double temp) {
+        this.temp = temp;
+    }
 }
