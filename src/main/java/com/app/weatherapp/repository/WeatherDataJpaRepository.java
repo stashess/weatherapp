fbe329710cf50fc7bc96a06c2e8e91d5fff21e7d
StanislavCheslavskyi <stas.hess@gmail.com>
2018-10-25 13:38:52 +0300

    repository

--- src/main/java/com/app/weatherapp/repository/WeatherDataJpaRepository.java
+++ src/main/java/com/app/weatherapp/repository/WeatherDataJpaRepository.java
@@ -3,9 +3,14 @@ package com.app.weatherapp.repository;
 import com.app.weatherapp.model.entity.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface WeatherDataJpaRepository extends JpaRepository<Weather,Long> {
 
     @Override
     Weather save(Weather weather);
+
+    List<Weather> findByDateIsGreaterThanEqualAndDateIsLessThanEqual(LocalDateTime startDate, LocalDateTime endDate);
 
 }
